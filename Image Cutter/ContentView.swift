7381c85efc03560e9861525a7b60e3a8a90ca845
Applichic <applichic@gmail.com>
2020-05-31 17:20:51 +0200

    Initial Commit

---
+++ Image Cutter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Image Cutter
+//
+//  Created by Lazyos on 5/31/20.
+//  Copyright © 2020 Lazyos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
